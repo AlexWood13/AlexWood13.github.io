3151614f8531b7bbfc9abd4ff7761a4a1df0a861
AlexWood13 <32333277+AlexWood13@users.noreply.github.com>
2017-10-28 22:25:50 +0100

    added script and testing menu

--- materialize/main.js
+++ materialize/main.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
   $('.parallax').parallax();
 });
 <!-- mobile menu -->
+$(document).ready(function(){
 $(".button-collapse").sideNav();
+});
+<!-- mobile menu two -->
+$(document).ready(function(){
+$(".dropdown-button").dropdown();
+});
