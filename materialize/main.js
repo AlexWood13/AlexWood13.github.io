1a72b18f634e392a4f4521ec66e999deff185ea6
AlexWood13 <32333277+AlexWood13@users.noreply.github.com>
2017-11-01 16:59:48 +0000

    modal footer
    added a modal. corrected text colour, changed images and fixed mobile interactivity.

--- materialize/main.js
+++ materialize/main.js
@@ -1,12 +1,20 @@
-<!-- parallax -->
+//parallax
 $(document).ready(function(){
   $('.parallax').parallax();
 });
-<!-- mobile menu -->
+//mobile menu
 $(document).ready(function(){
 $(".button-collapse").sideNav();
 });
-<!-- mobile menu two -->
+// mobile menu two
 $(document).ready(function(){
 $(".dropdown-button").dropdown();
 });
+//modal
+$(document).ready(function(){
+// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+   $('.modal').modal();
+ });
+//text form
+$('#textarea1').val('New Text');
+$('#textarea1').trigger('autoresize');
