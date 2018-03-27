89ad4dd5ca98986c6ff02e5a187f447d431de56b
AlexWood13 <32333277+AlexWood13@users.noreply.github.com>
2018-03-27 15:45:02 +0100

    Champions page code fix
    fixed all the code problems and ensured all links work

--- AJLeague/champions.php
+++ AJLeague/champions.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -34,7 +33,6 @@
            <a  href="index.html" class="brand-logo"><img src="assets/logo3.jpg" width="200" height="64"></a>
            <!-- mobile menu -->
            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-<ul class="right hide-on-med-and-down">
 
               <!-- buttons -->
               <ul id="nav-mobile" class="right hide-on-med-and-down">
@@ -60,7 +58,7 @@
 
 <!-- main-->
 <main>
-<?php include ("log")
+<?php include ("log.php")
 ?>
 
 <!-- section colour -->
@@ -79,19 +77,19 @@
         <!-- content -->
         <div class="card-content">
           <!-- text colour + icon image -->
-          <span class="card-title activator amber-text text-darken-1"> Gp <a href="gp.php" target="_blank"> <img src="assets/gpIcon.jpg" align="right"> </span>
+          <span class="card-title activator amber-text text-darken-1"> Gp <a href="gp.php" target="_blank"> <img src="assets/gpIcon.jpg" align="right">
             <!-- link text colour -->
-         <div class="linktext">Champion's page</div> </a>
+         <div class="linktext">Champion's page</div> </a> </span>
         </div>
         <!-- card revealed colour -->
         <div class="card-reveal blue-grey darken-4">
           <span class="card-title amber-text text-darken-1">Gangplank<i class="material-icons right red-text">close</i></span>
           <!-- change link here!!!! -->
-  <a href="gp.php" target="_blank" <p class="linktext">If you would like to know more information about Gangplank, click on any of the white text to go to Gangplank's Champion page. alternatively you can click the orange text or X to close this card. </p>
+  <a href="gp.php" target="_blank"> <p class="linktext">If you would like to know more information about Gangplank, click on any of the white text to go to Gangplank's Champion page. alternatively you can click the orange    text or X to close this card. </p> </a>
         </div>
       </div>
   </div>
-</a>
+
 <!-- end of card 1 -->
 
   <!-- card 2 Azir -->
@@ -104,19 +102,19 @@
         <!-- content -->
         <div class="card-content">
           <!-- text colour + icon image -->
-          <span class="card-title activator amber-text text-darken-1"> Azir <a href="azir.php" target="_blank"> <img src="assets/azirIcon.jpg" align="right"> </span>
+          <span class="card-title activator amber-text text-darken-1"> Azir <a href="azir.php" target="_blank"> <img src="assets/azirIcon.jpg" align="right">
             <!-- link text colour -->
-         <div class="linktext">Champion's page</div> </a>
+         <div class="linktext">Champion's page</div> </a> </span>
         </div>
         <!-- card revealed colour -->
         <div class="card-reveal blue-grey darken-4">
           <span class="card-title amber-text text-darken-1">Azir<i class="material-icons right red-text">close</i></span>
           <!-- change link here!!!! -->
-  <a href="azir.php" target="_blank"<p class="linktext">If you would like to know more information about Azir, click on any of the white text to go to Azir's Champion page. alternatively you can click the orange text or X to close this card.</p>
+  <a href="azir.php" target="_blank"> <p class="linktext">If you would like to know more information about Azir, click on any of the white text to go to Azir's Champion page. alternatively you can click the orange text or X to close this card.</p> </a>
         </div>
       </div>
   </div>
-</a>
+
 <!-- end of card 2 -->
 
   <!-- card 3 Kayn -->
@@ -129,19 +127,19 @@
         <!-- content -->
         <div class="card-content">
           <!-- text colour + icon image -->
-          <span class="card-title activator amber-text text-darken-1"> Kayn <a href="kayn.php" target="_blank"> <img src="assets/kaynIcon.jpg" align="right"> </span>
+          <span class="card-title activator amber-text text-darken-1"> Kayn <a href="kayn.php" target="_blank"> <img src="assets/kaynIcon.jpg" align="right">
             <!-- link text colour -->
-         <div class="linktext">Champion's page</div> </a>
+         <div class="linktext">Champion's page</div> </a> </span>
         </div>
         <!-- card revealed colour -->
         <div class="card-reveal blue-grey darken-4">
           <span class="card-title amber-text text-darken-1">Kayn<i class="material-icons right red-text">close</i></span>
           <!-- change link here!!!! -->
-  <a href="kayn.php" target="_blank" <p class="linktext">If you would like to know more information about Kayn, click on any of the white text to go to Kayn's Champion page. alternatively you can click the orange text or X to close this card.</p>
+  <a href="kayn.php" target="_blank"> <p class="linktext">If you would like to know more information about Kayn, click on any of the white text to go to Kayn's Champion page. alternatively you can click the orange text or X to close this card.</p> </a>
         </div>
       </div>
   </div>
-</a>
+
 <!-- end of card 3 -->
 
   <!-- card 4 Zed -->
@@ -154,19 +152,19 @@
         <!-- content -->
         <div class="card-content">
           <!-- text colour + icon image -->
-          <span class="card-title activator amber-text text-darken-1"> Zed <a href="zed.php" target="_blank"> <img src="assets/zedIcon.jpg" align="right"> </span>
+          <span class="card-title activator amber-text text-darken-1"> Zed <a href="zed.php" target="_blank"> <img src="assets/zedIcon.jpg" align="right">
             <!-- link text colour -->
-         <div class="linktext">Champion's page</div> </a>
+         <div class="linktext">Champion's page</div> </a>  </span>
         </div>
         <!-- card revealed colour -->
         <div class="card-reveal blue-grey darken-4">
           <span class="card-title amber-text text-darken-1">Zed<i class="material-icons right red-text">close</i></span>
           <!-- change link here!!!! -->
-  <a href="zed.php" target="_blank" <p class="linktext">If you would like to know more information about Zed, click on any of the white text to go to Zed's Champion page. alternatively you can click the orange text or X to close this card.</p>
+  <a href="zed.php" target="_blank"> <p class="linktext">If you would like to know more information about Zed, click on any of the white text to go to Zed's Champion page. alternatively you can click the orange text or X to close this card.</p> </a>
         </div>
       </div>
   </div>
-</a>
+
 <!-- end of card 4 -->
 
   <!-- card 5 Yasuo -->
@@ -179,19 +177,19 @@
         <!-- content -->
         <div class="card-content">
           <!-- text colour + icon image -->
-          <span class="card-title activator amber-text text-darken-1"> Yasuo <a href="yasuo.php" target="_blank"> <img src="assets/yasuoIcon.jpg" align="right"> </span>
+          <span class="card-title activator amber-text text-darken-1"> Yasuo <a href="yasuo.php" target="_blank"> <img src="assets/yasuoIcon.jpg" align="right">
             <!-- link text colour -->
-         <div class="linktext">Champion's page</div> </a>
+         <div class="linktext">Champion's page</div> </a> </span>
         </div>
         <!-- card revealed colour -->
         <div class="card-reveal blue-grey darken-4">
           <span class="card-title amber-text text-darken-1">Yasuo<i class="material-icons right red-text">close</i></span>
           <!-- change link here!!!! -->
-  <a href="yasuo.php" target="_blank" <p class="linktext">If you would like to know more information about Yasuo, click on any of the white text to go to Yasuo's Champion page. alternatively you can click the orange text or X to close this card.</p>
+  <a href="yasuo.php" target="_blank"> <p class="linktext">If you would like to know more information about Yasuo, click on any of the white text to go to Yasuo's Champion page. alternatively you can click the orange text or X to close this card.</p> </a>
         </div>
       </div>
   </div>
-</a>
+
 <!-- end of card 5 -->
 
   <!-- card 6 Jhin -->
@@ -204,19 +202,19 @@
         <!-- content -->
         <div class="card-content">
           <!-- text colour + icon image -->
-          <span class="card-title activator amber-text text-darken-1"> Jhin <a href="jhin.php" target="_blank"> <img src="assets/jhinIcon.jpg" align="right"> </span>
+          <span class="card-title activator amber-text text-darken-1"> Jhin <a href="jhin.php" target="_blank"> <img src="assets/jhinIcon.jpg" align="right">
             <!-- link text colour -->
-         <div class="linktext">Champion's page</div> </a>
+         <div class="linktext">Champion's page</div> </a> </span>
         </div>
         <!-- card revealed colour -->
         <div class="card-reveal blue-grey darken-4">
           <span class="card-title amber-text text-darken-1">Jhin<i class="material-icons right red-text">close</i></span>
           <!-- change link here!!!! -->
-  <a href="jhin.php" target="_blank" <p class="linktext">If you would like to know more information about Jhin, click on any of the white text to go to Jhin's Champion page. alternatively you can click the orange text or X to close this card.</p>
+  <a href="jhin.php" target="_blank"> <p class="linktext">If you would like to know more information about Jhin, click on any of the white text to go to Jhin's Champion page. alternatively you can click the orange text or X to close this card.</p> </a>
         </div>
       </div>
   </div>
-</a>
+
 <!-- end of card 6 -->
 
   <!-- card 7 Aurelion Sol -->
@@ -229,19 +227,19 @@
         <!-- content -->
         <div class="card-content">
           <!-- text colour + icon image -->
-          <span class="card-title activator amber-text text-darken-1"> A'Sol <a href="aSol.php" target="_blank"> <img src="assets/asolIcon.jpg" align="right"> </span>
+          <span class="card-title activator amber-text text-darken-1"> A'Sol <a href="aSol.php" target="_blank"> <img src="assets/asolIcon.jpg" align="right">
             <!-- link text colour -->
-         <div class="linktext">Champion's page</div> </a>
+         <div class="linktext">Champion's page</div> </a> </span>
         </div>
         <!-- card revealed colour -->
         <div class="card-reveal blue-grey darken-4">
           <span class="card-title amber-text text-darken-1">Aurelion Sol<i class="material-icons right red-text">close</i></span>
           <!-- change link here!!!! -->
-  <a href="aSol.php" target="_blank" <p class="linktext">If you would like to know more information about Aurelion Sol, click on any of the white text to go to Aurelion Sol's Champion page. alternatively you can click the orange text or X to close this card.</p>
+  <a href="aSol.php" target="_blank"> <p class="linktext">If you would like to know more information about Aurelion Sol, click on any of the white text to go to Aurelion Sol's Champion page. alternatively you can click the orange text or X to close this card.</p> </a>
         </div>
       </div>
   </div>
-</a>
+
 <!--  end of card 7 -->
 
   <!-- card 8 Camille -->
@@ -254,23 +252,20 @@
         <!-- content -->
         <div class="card-content">
           <!-- text colour + icon image -->
-          <span class="card-title activator amber-text text-darken-1"> Camille <a href="camille.php" target="_blank"> <img src="assets/camilleIcon.jpg" align="right"> </span>
+          <span class="card-title activator amber-text text-darken-1"> Camille <a href="camille.php" target="_blank"> <img src="assets/camilleIcon.jpg" align="right">
             <!-- link text colour -->
-         <div class="linktext">Champion's page</div> </a>
+         <div class="linktext">Champion's page</div> </a> </span>
         </div>
         <!-- card revealed colour -->
         <div class="card-reveal blue-grey darken-4">
           <span class="card-title amber-text text-darken-1">Camille<i class="material-icons right red-text">close</i></span>
           <!-- change link here!!!! -->
-  <a href="camille.php" target="_blank" <p class="linktext">If you would like to know more information about Camille, click on any of the white text to go to Camille's Champion page. alternatively you can click the orange text or X to close this card.</p>
+  <a href="camille.php" target="_blank"> <p class="linktext">If you would like to know more information about Camille, click on any of the white text to go to Camille's Champion page. alternatively you can click the orange text or X to close this card.</p> </a>
         </div>
       </div>
   </div>
-</a>
 <!-- end of card 8 -->
 
-<!-- row container ending tag -->
-</div>
 
 <!-- back to top link  -->
   <div class="center">
@@ -279,6 +274,7 @@
 
 <!--section ending tag -->
  </div>
+	</div>
 
 <!-- main ending -->
 </main>
