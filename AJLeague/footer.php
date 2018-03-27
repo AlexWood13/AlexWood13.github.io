994c606bf2de63d23097acc56fc9ec696ee4c1b9
AlexWood13 <32333277+AlexWood13@users.noreply.github.com>
2018-03-27 17:41:21 +0100

    updated footer

--- AJLeague/footer.php
+++ AJLeague/footer.php
@@ -1,12 +1,13 @@
 <!-- footer -->
 <footer>
+
 <div class="section blue-grey darken-4">
       <footer class="page-footer blue-grey">
         <div class="container">
           <div class="row">
             <div class="col l6 s12">
               <h5 class="white-text">Created by: Alex Wood</h5>
-              <p class="grey-text text-lighten-4">Find the game here: <a href="https://euw.leagueoflegends.com/en/game-info/" target="_blank"> League of Legends</p> </a>
+              <p class="grey-text text-lighten-4">Find the game here: <a href="https://euw.leagueoflegends.com/en/game-info/" target="_blank"> League of Legends </a> </p>
             </div>
             <div class="col l4 offset-l2 s12">
               <h5 class="white-text">External Links</h5>
@@ -16,61 +17,13 @@
                 <a class="grey-text text-lighten-3" href="https://twitter.com/AjW1837284" target="_blank"><img src="assets/twitter.jpg"></a>
                 <a class="grey-text text-lighten-3" href="https://www.twitch.tv/farquaadishere" target="_blank"><img src="assets/twitch.jpg"></a>
                 <a class="grey-text text-lighten-3" href="https://plays.tv/u/FarquaadOgreLoad" target="_blank"><img src="assets/playsTv.png"></a>
-
             </div>
           </div>
         </div>
 
+
         <!-- footer copyright -->
-        <div class="footer-copyright">
-          <div class="container">
+		  <div class="footer-copyright"> </div>
 
-
-
-<!-- modal -->
-<div class="right">
-
-          <!-- Modal Trigger -->
-<a class="waves-effect waves-light btn modal-trigger blue-grey darken-4" href="#modal1">Contact Us</a>
-
-<!-- Modal Structure -->
-
-<div id="modal1" class="modal">
-  <div class="modal-content">
-    <div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <i class="material-icons prefix">account_circle</i>
-          <input id="icon_prefix" type="text" class="formtext validate">
-          <label for="icon_prefix">Username</label>
-        </div>
-        <div class="input-field col s6">
-          <i class="material-icons prefix">email</i>
-          <input id="icon_prefix" type="text" class="formtext validate">
-          <label for="icon_prefix">email</label>
-        </div>
-      </div>
-      <!-- text area -->
-      <div class="row">
-       <form class="col s12">
-         <div class="row">
-           <div class="input-field col s12">
-               <i class="material-icons prefix">edit</i>
-               <input id="icon_prefix" type="text" class="formtext validate">
-             <label for="textarea1">Issue</label>
-           </div>
-         </div>
-       </form>
-     </div>
-     <!-- submit button + cancel  -->
-        </div>
-        <div class="modal-footer">
-          <button class="btn waves-effect waves-light blue-grey darken-4" type="submit" name="action">Submit
-          <i class="material-icons right">send</i>
-        </button>
-     <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cancel</a>
-   </div>
- </div>
+       <!-- end of footer -->
       </footer>
-<!-- end of footer -->
