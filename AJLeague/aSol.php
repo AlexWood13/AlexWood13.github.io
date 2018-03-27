e3aae919d1dff11e6aba8483c1f31bda5f046a36
AlexWood13 <32333277+AlexWood13@users.noreply.github.com>
2018-03-27 15:52:13 +0100

    asol page update

--- AJLeague/aSol.php
+++ AJLeague/aSol.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -34,27 +33,26 @@
 
            <!-- mobile menu -->
            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-<ul class="right hide-on-med-and-down">
+>
 
               <!-- buttons -->
               <ul id="nav-mobile" class="right hide-on-med-and-down">
-             <li><a href="index.html" class="waves-effect waves-light btn  blue-grey darken-4">Home</a></li>
-             <li><a href="champions.html" class="waves-effect waves-light btn blue-grey darken-4">Champions</a></li>
-             <li><a href="Tactics.html"class="waves-effect waves-light btn blue-grey darken-4">Tactics and Strategy</a></li>
+             <li><a href="index.php" class="waves-effect waves-light btn  blue-grey darken-4">Home</a></li>
+             <li><a href="champions.php" class="waves-effect waves-light btn blue-grey darken-4">Champions</a></li>
+             <li><a href="Tactics.php"class="waves-effect waves-light btn blue-grey darken-4">Tactics and Strategy</a></li>
              <li><a class="waves-effect waves-light btn blue-grey darken-4">Discussion</a></li>
            </ul>
-         </div>
+
                     <!-- mobile menu -->
                    <ul class="side-nav" id="mobile-demo">
           <li><a href="index.html">Home</a></li>
-          <li><a href="champions.html">Champions</a></li>
-          <li><a href="Tactics.html">Tactics and Strategy</a></li>
-          <li><a href="discussion.html">Discussion</a></li>
+          <li><a href="champions.php">Champions</a></li>
+          <li><a href="Tactics.php">Tactics and Strategy</a></li>
+          <li><a href="discussion.php">Discussion</a></li>
           </ul>
           </div>
-                 </nav>
+                </nav>
                   <!-- end of navar -->
-
 
       <!-- header-->
       </header>
@@ -62,19 +60,13 @@
 <!-- main-->
 <main>
 
-  <!-- sign in and sign up buttons-->
-  <div class ="row-container center">
-   <!-- Sign in -->
-
-   <!-- Modal Trigger -->
-   <div class="section blue-grey">
-   <a class="waves-effect waves-light btn modal-trigger blue-grey darken-4" href="#modal3">Sign in</a>
+	<!-- login system -->
+ 	  <?php include("log.php")
+	?>
 
     <!-- Modal Structure -->
-
     <div id="modal3" class="modal">
       <div class="modal-content">
-        <div class="row">
         <form class="col s12">
           <div class="row">
             <div class="input-field col s6">
@@ -88,7 +80,7 @@
               <label for="icon_prefix">Password</label>
             </div>
           </div>
-       </div>
+
 
          <!-- submit button + cancel  -->
 
@@ -97,8 +89,7 @@
             </button>
          <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cancel</a>
        </div>
-     </div>
-   </div>
+ 	</div>
 
            <!-- sign up -->
 
@@ -193,99 +184,18 @@
     <a class="carousel-item" href="#five!"><img src="assets/aS5.jpg" height="200px" width="250px"></a>
   </div>
 
-
-
-
-
   <!-- back to top link  -->
     <div class="center">
   <a href="#top" class="waves-effect waves-light btn  blue-grey darken-4">Back to top</a>
    </div>
-
-
-
-
 </div>
 <!-- main ending -->
 </main>
 
 
 <!-- footer -->
-<footer>
-<div class="section blue-grey darken-4">
-      <footer class="page-footer blue-grey">
-        <div class="container">
-          <div class="row">
-            <div class="col l6 s12">
-              <h5 class="white-text">Created by: Alex Wood</h5>
-              <p class="grey-text text-lighten-4">Find the game here: <a href="https://euw.leagueoflegends.com/en/game-info/" target="_blank"> League of Legends</p> </a>
-            </div>
-            <div class="col l4 offset-l2 s12">
-              <h5 class="white-text">External Links</h5>
-
-              <!-- External link Icons -->
-              <a href="https://www.youtube.com/channel/UCvFnfTKhoV8CUyyQxaHK6uw/featured?view_as=subscriber" target="_blank"><img src="assets/youtube.jpg"></a>
-              <a class="grey-text text-lighten-3" href="https://twitter.com/AjW1837284" target="_blank"><img src="assets/twitter.jpg"></a>
-              <a class="grey-text text-lighten-3" href="https://www.twitch.tv/farquaadishere" target="_blank"><img src="assets/twitch.jpg"></a>
-              <a class="grey-text text-lighten-3" href="https://plays.tv/u/FarquaadOgreLoad" target="_blank"><img src="assets/playsTv.png"></a>
-
-            </div>
-          </div>
-        </div>
-
-        <!-- footer copyright -->
-        <div class="footer-copyright">
-          <div class="container">
-
-
-
-<!-- modal -->
-<div class="right">
-
-          <!-- Modal Trigger -->
-<a class="waves-effect waves-light btn modal-trigger blue-grey darken-4" href="#modal1">Contact Us</a>
-
-<!-- Modal Structure -->
-
-<div id="modal1" class="modal">
-  <div class="modal-content">
-    <div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <i class="material-icons prefix">account_circle</i>
-          <input id="icon_prefix" type="text" class="formtext validate">
-          <label for="icon_prefix">Username</label>
-        </div>
-        <div class="input-field col s6">
-          <i class="material-icons prefix">email</i>
-          <input id="icon_prefix" type="text" class="formtext validate">
-          <label for="icon_prefix">email</label>
-        </div>
-      </div>
-      <!-- text area -->
-      <div class="row">
-       <form class="col s12">
-         <div class="row">
-           <div class="input-field col s12">
-               <i class="material-icons prefix">edit</i>
-               <input id="icon_prefix" type="text" class="formtext validate">
-             <label for="textarea1">Issue</label>
-           </div>
-         </div>
-       </form>
-     </div>
-     <!-- submit button + cancel  -->
-        </div>
-        <div class="modal-footer">
-          <button class="btn waves-effect waves-light blue-grey darken-4" type="submit" name="action">Submit
-          <i class="material-icons right">send</i>
-        </button>
-     <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cancel</a>
-   </div>
- </div>
-      </footer>
-<!-- end of footer -->
+<?php include ("footer.php")
+	?>
 
   </body>
 </html>
